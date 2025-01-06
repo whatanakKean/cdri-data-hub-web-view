0bc7bf3ce224f31bba5fbd4005e0264d55778439
Kean Whatanak <keanwhatanak@gmail.com>
2025-01-06 11:29:57 +0700

    Add Data Unpivoting & Fix Footer

--- src/components/footer.py
+++ src/components/footer.py
@@ -4,12 +4,12 @@ import dash_mantine_components as dmc
 def footer():
     return html.Div(
         style={
-            "position": "relative",  # Set position to relative
-            "bottom": "0",  # Pin the footer to the bottom of the page
-            "width": "100%",  # Ensure the footer spans the full width of the page
+            "position": "relative",
+            "bottom": "0",
+            "width": "100%",
             "backgroundColor": "#336666",
             "color": "white",
-            "padding": "5px"
+            "padding": "20px"
         },
         children=[
             dmc.Grid(
@@ -30,19 +30,63 @@ def footer():
                         span={"base": 12, "md": 4},
                     ),
                     dmc.GridCol(
-                        dcc.Markdown(
-                            """
-                            **CDRI Data Hub** is a centralized repository for research-related data, offering reliable information across various sectors in Cambodia. It supports evidence-based decision-making by providing datasets, visualization tools, and resources tailored to researchers, policymakers, and practitioners.
-                            """,
-                            style={
-                                "marginTop": "1rem",
-                                "color": "white",  # Text color
-                                "fontSize": "14px",  # Optional font size
-                            },
-                        ),
-                        span={"base": 12, "md": 8},
+                        children=[
+                            dmc.Title("Phone:", order=4, td="underline"),
+                            dmc.Text("+855 23 881701"),
+                            dmc.Text("+855 23 881916"),
+                            dmc.Text("+855 23 883603"),
+                            dmc.Space(h=10),
+                            
+                            dmc.Title("Email:", order=4, td="underline"),
+                            dmc.Text("cdri@cdri.org.kh"),
+                            dmc.Space(h=10),
+                            
+                            # dmc.Title("Address:", order=4, td="underline"),
+                            # dmc.Text("#56 Street 315, Tuol Kork, Phnom Penh, Cambodia"),
+                            # dmc.Text("PO Box 622"),
+                            # dmc.Text("Postal Code: 120508"),
+                        ],
+                        span={"base": 12, "md": 4},
                     ),
+                    dmc.GridCol(
+                        children=[
+                            dmc.Title("Sectors: ", order=4, td="underline"),
+                            dmc.Box(
+                                children=[
+                                    dmc.Anchor(
+                                        ">     Agriculture and Rural Development",
+                                        href="/agriculture-and-rural-development",
+                                        style={"color": "white", "display": "block"}
+                                    ),
+                                    dmc.Anchor(
+                                        ">     Development Economics and Trade",
+                                        href="/development-economics-and-trade",
+                                        style={"color": "white", "display": "block"}
+                                    ),
+                                    dmc.Anchor(
+                                        ">     Educational Research and Innovation",
+                                        href="/educational-research-and-innovation",
+                                        style={"color": "white", "display": "block"}
+                                    ),
+                                    dmc.Anchor(
+                                        ">     Natural Resource and Environment",
+                                        href="/natural-resource-and-environment",
+                                        style={"color": "white", "display": "block"}
+                                    ),
+                                    dmc.Anchor(
+                                        ">     Governance and Inclusive Society",
+                                        href="/governance-and-inclusive-society",
+                                        style={"color": "white", "display": "block"}
+                                    ),
+                                ]
+                            )
+                        ],
+                        span={"base": 12, "md": 4},
+                    )
+
                 ],
             ),
+            dmc.Divider(m=10),
+            dmc.Text("Copyright © 2025 CDRI. All rights reserved.", ta="center"),
         ],
     )
